873880ad428a1605804be79a050ec1aa32858230
Tom Dunn <tdunn@artofcontext.com>
2019-04-09 01:33:04 -0400

    Lightsail launch script and docuer compose systemd service config

---
+++ lightsail-setup/lightsail-compose.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# install latest version of docker the lazy way
+curl -sSL https://get.docker.com | sh
+
+# make it so you don't need to sudo to run docker commands
+usermod -aG docker ubuntu
+
+# install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# copy in systemd unit file and register it so our compose file runs 
+# on system restart
+curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercontent.com/mikegcoleman/todo/master/docker-compose-app.service
+systemctl enable docker-compose-app
