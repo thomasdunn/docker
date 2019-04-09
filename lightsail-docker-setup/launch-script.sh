a2e84bbbf8fa1e7ccdb8bd482000786ee2b55aad
Tom Dunn <tdunn@artofcontext.com>
2019-04-09 02:11:29 -0400

    Added lightsail instance launch script

---
+++ lightsail-docker-setup/launch-script.sh
@@ -0,0 +1,5 @@
+curl -o lightsail-compose.sh https://raw.githubusercontent.com/thomasdunn/docker/master/lightsail-docker-setup/lightsail-compose.sh
+
+chmod +x ./lightsail-compose.sh
+
+./lightsail-compose.sh
